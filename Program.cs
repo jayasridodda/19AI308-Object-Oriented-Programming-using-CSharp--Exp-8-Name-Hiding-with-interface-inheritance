5ee1248fa13b0e9b700a1e87d3ea4fb021973bb4
jayasridodda <doddajayasri2482@gmail.com>
2024-05-08 11:09:12 +0530

    updated aim, result and partial algorithm

--- Program.cs
+++ Program.cs
@@ -1,36 +1,30 @@
 using System;
-
-// Define the IMario interface
 public interface IMario
 {
- 
-}
+    void Ability();
 
-// Implement the IMario interface in the Mario class
-public class Mario : IMario
-{
+}
+class Mario:IMario{
     public virtual void Ability()
     {
-  
+        Console.WriteLine("Mario's Ability:Normal Dancing");
     }
-}
-
-// SuperMario class that inherits from Mario and hides the Ability() method
-public class SuperMario : Mario
+}  
+class SuperMario:Mario
 {
     public new void Ability()
     {
-    
+        Console.WriteLine("Mario's Ability:Super Dancing");
     }
 }
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[]args)
     {
-       
+        IMario mario=new Mario();
+        SuperMario superMario=new SuperMario();
+        mario.Ability();
+        superMario.Ability();
 
-       
     }
 }
-
